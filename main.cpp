c01e73510a4a94cbf19515709e4e8b072da157ba
chanyoung1557 <158740164+chanyoung1557@users.noreply.github.com>
2024-04-02 19:40:40 +0900

    database

--- main.cpp
+++ main.cpp
@@ -15,40 +15,48 @@ Array* inputArray() {
     cout << "type (int, double, string, array): "; // 항목 타입 입력 요청
     string itemTypeStr;
     cin >> itemTypeStr; // 항목 타입 입력 받기
-    for (int i = 0; i < array->size; ++i) {
         // 항목 타입에 따라 처리
-        if (itemTypeStr == "int")
-        {
-            int* value = new int[array->size]; // int 형 값에 대한 포인터 동적 할당
+    if (itemTypeStr == "int")
+    {
+        array->type = INT;
+        int* value = new int[array->size]; // int 형 값에 대한 포인터 동적 할당
+        for (int i = 0; i < array->size; ++i) {
             int a = 0;
+            cout << "item["<<i<<"]: ";
             cin >> a;
             value[i] = a;
             array->items = value;
-            //array->items = value; // 배열의 항목에 값 할당
-            array->type = INT;
         }
-        else if (itemTypeStr == "double") {
-            double* value = new double[array->size]; // double 형 값에 대한 배열 동적 할당
+        //array->items = value; // 배열의 항목에 값 할당
+    }
+    else if (itemTypeStr == "double") {
+        array->type = DOUBLE;
+        double* value = new double[array->size]; // double 형 값에 대한 배열 동적 할당
+        for (int i = 0; i < array->size; ++i) {
             double a = 0;
+            cout << "item[" << i << "]: ";
             cin >> a;
             value[i] = a;
             array->items = value; // 배열의 항목에 값 할당
-            array->type = DOUBLE;
         }
-        else if (itemTypeStr == "string") {
-            string* value = new string[array->size]; // 문자열 값에 대한 배열 동적 할당
-
+    }
+    else if (itemTypeStr == "string") {
+        array->type = STRING;
+        string* value = new string[array->size]; // 문자열 값에 대한 배열 동적 할당
+        for (int i = 0; i < array->size; ++i) {
             cin.ignore(); // 이전 cin 입력의 개행 문자를 무시
+            cout << "item[" << i << "]: ";
             getline(cin, value[i]); // 공백을 포함한 문자열 입력 받기
             array->items = value; // 배열의 항목에 값 할당
-            array->type = STRING;
         }
-        else if (itemTypeStr == "array") {
-            // 재귀적으로 배열 입력 받기
+    }
+    else if (itemTypeStr == "array") {
+        array->type = ARRAY;
+        // 재귀적으로 배열 입력 받기
+        for (int i = 0; i < array->size; ++i) {
             Array* subArray = inputArray();
             if (subArray != nullptr) {
                 array->items = (void*)subArray; // 배열의 항목에 배열 포인터 할당
-                array->type = ARRAY;
             }
             else {
                 delete[] array->items; // 동적 할당된 배열 메모리 해제
@@ -56,8 +64,8 @@ Array* inputArray() {
                 return nullptr; // nullptr 반환
             }
         }
-
     }
+
 
     return array; // 입력된 배열 포인터 반환
 }
@@ -69,7 +77,8 @@ void printEntry(void* value, Type type, int size = 0) {
         if (data != nullptr) { // 유효한 포인터인 경우
             if (size != 0) { // 배열의 경우
                 for (int i = 0; i < size; ++i) {
-                    std::cout << data[i] << std::endl; // 배열 요소 출력
+                    std::cout << data[i]; // 배열 요소 출력
+                    if (i != size-1) std::cout << ", ";
                 }
             }
             else {
@@ -86,7 +95,8 @@ void printEntry(void* value, Type type, int size = 0) {
         if (data != nullptr) {
             if (size != 0) {
                 for (int i = 0; i < size; ++i) {
-                    std::cout << data[i] << std::endl;
+                    std::cout << data[i];
+                    if (i != size-1) std::cout << ", ";
                 }
             }
             else {
@@ -111,11 +121,8 @@ void printEntry(void* value, Type type, int size = 0) {
     case ARRAY: {
         Array* arr = static_cast<Array*>(value); // ARRAY 타입의 값을 Array 포인터로 캐스팅
         if (arr != nullptr) {
-            std::cout << "array -> [";
-            for (int i = 0; i < arr->size; ++i) {
-                if (i > 0) std::cout << ", ";
-                printEntry(arr->items, arr->type, arr->size); // 배열의 항목 출력
-            }
+            std::cout << "[";
+            printEntry(arr->items, arr->type, arr->size); // 배열의 항목 출력
             std::cout << "]" << std::endl;
         }
         else {
@@ -178,7 +185,7 @@ int main() {
             }
             else if (valueType == "array") {
                 type = ARRAY; // 입력된 값의 타입이 array일 경우
-                Array* array = inputArray(); // 배열 입력 받기
+                Array* array= inputArray(); // 배열 입력 받기
                 if (array != nullptr) { // 유효한 배열이 입력된 경우
                     add(database, create(type, key, (void*)array)); // 데이터베이스에 새 항목 추가
                 }
